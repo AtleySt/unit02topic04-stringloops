507de68a793ef945c44c36ead6347c53b053f469
Atley St. Vrain <atleystvrain@gmail.com>
2023-02-13 12:04:21 -0600

    hasRepeatedConsecutives | Complete

--- StringLoops.java
+++ StringLoops.java
@@ -26,8 +26,18 @@ public class StringLoops {
     return result;
   }
 
+  public static boolean hasRepeatedConsecutives(String word) {
+    for (int pos = 1; pos < word.length(); pos++) {
+      if (word.substring(pos,pos+1).equals(word.substring(pos-1,pos))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
     //System.out.println(reverseCharacters("taco cat"));
-    System.out.println(Arrays.toString(indexesOfAll("attack", "a")));
+    //System.out.println(Arrays.toString(indexesOfAll("attack", "a")));
+    System.out.println(hasRepeatedConsecutives("rook"));
   }
 }
