9fbc4f72d90cdeb2d370e96aa36f4a6b6c9911b0
Atley St. Vrain <atleystvrain@gmail.com>
2023-02-13 11:46:11 -0600

    indexesOfAll | Incomplete (so far)

--- StringLoops.java
+++ StringLoops.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringLoops {
 
   public static String reverseCharacters(String word) {
@@ -8,8 +10,24 @@ public class StringLoops {
     return result;
   }
 
+  public static int[] indexesOfAll(String text, String searchTerm) {
+    int searchTermCount = 0;
+    for (int pos = 0; pos < text.length(); pos++) {
+      if (text.substring(pos,pos+1).equals(searchTerm)) {
+        searchTermCount++;
+      }
+    }
+    int[] result = new int[searchTermCount];
+    for (int pos = 0; pos < text.length(); pos++) {
+      if (text.substring(pos,pos+1).equals(searchTerm)) {
+        result[pos] = pos;
+      }
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
-    System.out.println(reverseCharacters("taco cat"));
-    
+    //System.out.println(reverseCharacters("taco cat"));
+    System.out.println(Arrays.toString(indexesOfAll("attack", "a")));
   }
 }
